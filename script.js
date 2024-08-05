ca587e8e9b926dd9c005562415ce20d016e4f228
codeAlphaBravosite <help.factchannel@gmail.com>
2024-08-05 13:53:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -34,19 +34,28 @@ function formatNumber(number) {
 function displayCards(data) {
     const cardsContainer = document.getElementById('cards');
     cardsContainer.innerHTML = '';
-    data.forEach((video) => {
+    data.forEach((video, index) => {
         const card = document.createElement('div');
         card.className = 'card';
+        card.dataset.index = index;
         const thumbnailUrl = `https://img.youtube.com/vi/${video['Video Id']}/0.jpg`;
         card.innerHTML = `
             <img src="${thumbnailUrl}" alt="${video['Title']}" class="thumbnail">
             <div class="card-content">
                 <a href="https://www.youtube.com/watch?v=${video['Video Id']}" target="_blank" class="card-title">${video['Title']}</a>
                 <div class="card-subtitle">${formatNumber(video['Views'])} views</div>
+                <label class="viewed-checkbox">
+                    <input type="checkbox" onchange="toggleVisibility(${index})"> Viewed?
+                </label>
             </div>
         `;
         cardsContainer.appendChild(card);
     });
+}
+
+function toggleVisibility(index) {
+    const card = document.querySelector(`.card[data-index='${index}']`);
+    card.classList.toggle('viewed');
 }
 
 // Dark mode toggle
